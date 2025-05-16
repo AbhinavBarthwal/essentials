0f337271bb231ca343ef65e0eb973a1b3f817aae
Abhinan Barthwal <134462974+AbhinavBarthwal@users.noreply.github.com.>
2025-05-16 20:25:20 +0530

    h

--- src/components/todaybrief.jsx
+++ src/components/todaybrief.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Route,Router,Routes } from "react-router";
 
-function todaysbrief()
-{
- return
- (
-    <>
-    <div className="w-80vw">
+const TodaysBrief = () => {
+  return (
+   <div className="w-100 h-100 flex items-center align-middle justify-center">
+      <div className="w-80vw h-full bg-yellow-700">
+      Lorem ipsum dolor sit <hr />amet consectetur adipisicing elit. Nobis, quisquam.
+      </div>
+   </div>
+  );
+};
 
-    </div>
-    </>
- )
-}
-
-export default todaysbrief;
-
+export default TodaysBrief;
